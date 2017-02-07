b6c411d19b2a4bae60e6c40ad341d419ed03a0da
JoseAvalos <javalos@CI-38>
2017-02-07 08:25:28 -0500

    ADD pow function

--- SPI/SPI.ino
+++ SPI/SPI.ino
@@ -1,98 +1,140 @@
 #include <SPI.h>
 #include "BigNumber.h"
-#include <math.h>
-
 
 #define CS PA_5
-#define UDCLK PM_6
+#define UDCLK PD_2
+#define IO_RESET PA_4
+
 #define DDS_NBITS  48
-#define INT64_C  0
 
-
-
-int SPI_delay = 1;
+int SPI_delay = 150;
 
 byte resp6[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 byte resp4[] = {0x0, 0x0, 0x0, 0x0};
 
 void setup() {
-  BigNumber::begin (); 
-  Serial.begin(115200);
+  //Inicialization
+  BigNumber::begin();
+  //Serial init
+  Serial.begin(115200); \
+  Serial.println("#####################################");
+  //SPI general configuration
   SPI.setModule(1);
   SPI.setBitOrder(MSBFIRST);
   SPI.begin();
-
-  pinMode(UDCLK, OUTPUT);
+  //PIN
   pinMode(CS, OUTPUT);
+  pinMode(UDCLK, OUTPUT);
+  pinMode(IO_RESET, OUTPUT);
+  //Set off
+  off(CS);
+  off(UDCLK);
+  delayMicroseconds(100);
+  on(CS);
+  delayMicroseconds(100);
 
-  udclk_off();
-  cs_on();
-  delayMicroseconds(500);
+  //Set initial to DDS using SPI
+  off(CS);
+  SPI.transfer(0x07);
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(0x10);
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(0x64);
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(0x00);
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(0x21);
+  on(CS);
 
-  //Configuracion del SPI `
-  cs_off();
-  SPI.transfer(0x07);//07 //E0
+  on(IO_RESET);
   delayMicroseconds(SPI_delay);
-  SPI.transfer(0x10);//10 // 08
+  off(IO_RESET);
   delayMicroseconds(SPI_delay);
-  SPI.transfer(0x64);//64 //26
+
+  off(CS);
+  SPI.transfer(0x87);
   delayMicroseconds(SPI_delay);
-  SPI.transfer(0x01);//01 //80
+  resp4[0] = SPI.transfer(0x00);
   delayMicroseconds(SPI_delay);
-  SPI.transfer(0x21);//21 //84
-  cs_on();
-  delayMicroseconds(500);
+  resp4[1] = SPI.transfer(0x00);
+  delayMicroseconds(SPI_delay);
+  resp4[2] = SPI.transfer(0x00);
+  delayMicroseconds(SPI_delay);
+  resp4[3] = SPI.transfer(0x00);
+  delayMicroseconds(SPI_delay);
+  on(CS);
+
+  Serial.println("Read information set: ");
+  Serial.print("[ ");
+  Serial.print(resp4[0], HEX);
+  Serial.print(" ,");
+  Serial.print(resp4[1], HEX);
+  Serial.print(" ,");
+  Serial.print(resp4[2], HEX);
+  Serial.print(" ,");
+  Serial.print(resp4[3], HEX);
+  Serial.println(" ]");
 
 
-//
+
+  delayMicroseconds(100);
+
+  //SET frequency
   byte *freq ;
-  freq = freq2binary(600, 60000000);
-//
+  freq = freq2binary(100000, 60000000);
+  delayMicroseconds(10);
 
-  cs_off();
+  off(CS);
+  delayMicroseconds(10);
+  off(UDCLK);
+  delayMicroseconds(10);
+
+  on(IO_RESET);
+  delayMicroseconds(10000);
+  off(IO_RESET);
+  delayMicroseconds(SPI_delay);
+
+
+  on(UDCLK);
+  delayMicroseconds(SPI_delay);
+  off(UDCLK);
+  delayMicroseconds(SPI_delay);
+
+
+
   SPI.transfer(0x02);
-  delayMicroseconds(5);
-  udclk_on();
   delayMicroseconds(SPI_delay);
-  SPI.transfer(*(freq + 5));
-  delayMicroseconds(SPI_delay);
-  SPI.transfer(*(freq + 4));
-  delayMicroseconds(SPI_delay);
-  SPI.transfer(*(freq + 3));
-  delayMicroseconds(SPI_delay);
-  SPI.transfer(*(freq + 2));
+  SPI.transfer(*(freq + 0));
   delayMicroseconds(SPI_delay);
   SPI.transfer(*(freq + 1));
   delayMicroseconds(SPI_delay);
-  SPI.transfer(*(freq + 0));
-  udclk_off();
+  SPI.transfer(*(freq + 2));
   delayMicroseconds(SPI_delay);
-  cs_on();
-  delayMicroseconds(10);
+  SPI.transfer(*(freq + 3));
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(*(freq + 4));
+  delayMicroseconds(SPI_delay);
+  SPI.transfer(*(freq + 5));
+  delayMicroseconds(SPI_delay);
+  on(CS);
+  delayMicroseconds(SPI_delay);
 
- // while (1) {
-    cs_off();
-    SPI.transfer(0x87);
-    delayMicroseconds(SPI_delay);
-    resp4[0] = SPI.transfer(0x00);
-    delayMicroseconds(SPI_delay);
-    resp4[1] = SPI.transfer(0x00);
-    delayMicroseconds(SPI_delay);
-    resp4[2] = SPI.transfer(0x00);
-    delayMicroseconds(SPI_delay);
-    resp4[3] = SPI.transfer(0x00);
-    delayMicroseconds(SPI_delay);
-    cs_on();
-
-    Serial.println("************************************************");
-    Serial.println("Status: ");
-    Serial.println(resp4[0], HEX);
-    Serial.println(resp4[1], HEX);
-    Serial.println(resp4[2], HEX);
-    Serial.println(resp4[3], HEX);
+  on(IO_RESET);
+  delayMicroseconds(10000);
+  off(IO_RESET);
+  delayMicroseconds(SPI_delay);
 
 
-    cs_off();
+}
+
+void loop() {
+
+
+
+
+
+  /*
+    off(CS);
     SPI.transfer(0x82);
     delayMicroseconds(SPI_delay);
     resp6[0] = SPI.transfer(0x00);
@@ -107,94 +149,75 @@ void setup() {
     delayMicroseconds(SPI_delay);
     resp6[5] = SPI.transfer(0x00);
     delayMicroseconds(SPI_delay);
-    cs_on();
+    on(CS);
 
-    Serial.println("************************************************");
-    Serial.println("Frecuency Tuning Word: ");
-    Serial.println(resp6[0], HEX);
-    Serial.println(resp6[1], HEX);
-    Serial.println(resp6[2], HEX);
-    Serial.println(resp6[3], HEX);
-    Serial.println(resp6[4], HEX);
-    Serial.println(resp6[5], HEX);
- // }
+    Serial.println("Read frecuency Tuning Word: ");
+    Serial.print("[ ");
+    Serial.print(resp6[0], HEX);
+    Serial.print(" ,");
+    Serial.print(resp6[1], HEX);
+    Serial.print(" ,");
+    Serial.print(resp6[2], HEX);
+    Serial.print(" ,");
+    Serial.print(resp6[3], HEX);
+    Serial.print(" ,");
+    Serial.print(resp6[4], HEX);
+    Serial.print(" ,");
+    Serial.print(resp6[5], HEX);
+    Serial.println(" ]");
+  */
 }
 
-void loop() {
-  //
-}
-
-
-
-void cs_on() {
-
-  digitalWrite(CS, HIGH);
-}
-
-
-
-void cs_off() {
-
-  digitalWrite(CS, LOW);
-}
-
-
-
-void udclk_on() {
-  digitalWrite(UDCLK, HIGH);
-}
-
-
-void udclk_off() {
-
-  digitalWrite(UDCLK, LOW);
-}
-
-byte * freq2binary(float freq, float mclock) {
-  //long long int a=1;
+byte *freq2binary(float freq, float mclock) {
   static byte bytevalue[6] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-  BigNumber a;
-  a=pow64bits(2,5);
-  
-  Serial.println(a);
-  
-  /*
-  double x=a/pow64bits(2,45);
-  //double hola=pow(2,32);
-  //Serial.println(hola);
-  Serial.println(x);
-  
-  
+  BigNumber DesiredOut = freq;
+  BigNumber SYSCLK = mclock;
+  BigNumber a = 0;
+  BigNumber b = 256; // 2 bytes
+  BigNumber x = 1;
+  a = DesiredOut * pow64bits(2, 48) / SYSCLK;
   int n = 0;
-  while (a!= 0) {
-    bytevalue[n] =  byte(fmod(a,16));
-    Serial.println(bytevalue[n]);
-    a= round( a/ 16);
+  //Serial.print("[ ");
+
+  while (a != 0) {
+    bytevalue[n] =  byte(a % b);
+    a =  a / b;
+    //Serial.print(bytevalue[n], HEX);
+    //Serial.print(", ");
     n++;
   }
+  //Serial.println(" ]");
+  /*
+    Serial.println("Write frecuency Tuning Word: ");
+    Serial.print("[ ");
+    Serial.print(bytevalue[0], HEX);
+    Serial.print(" ,");
+    Serial.print(bytevalue[1], HEX);
+    Serial.print(" ,");
+    Serial.print(bytevalue[2], HEX);
+    Serial.print(" ,");
+    Serial.print(bytevalue[3], HEX);
+    Serial.print(" ,");
+    Serial.print(bytevalue[4], HEX);
+    Serial.print(" ,");
+    Serial.print(bytevalue[5], HEX);
+    Serial.println(" ]");
+    return bytevalue;
   */
-  return bytevalue;
 }
 
-
-BigNumber pow64bits(int a, int b){
-  
-  char  hola[1];
-  String a_string=String(a);
-  a_string.toCharArray(hola,1) ;
-  Serial.println(hola);
-  
-  BigNumber result="1";
-
-  BigNumber base=hola;
- 
-  
-  for (int i=0;i<b;i++){
-    
-    result=result*base;
-    Serial.println(result);
-    }
-  
-  return result;
-  
+BigNumber pow64bits(int a, int b) {
+  BigNumber result = 1;
+  BigNumber base = a;
+  for (int i = 0; i < b; i++) {
+    result = result * base;
   }
+  return result;
+}
+
+void on(int x) {
+  digitalWrite(x, HIGH);
+}
+void off(int x) {
+  digitalWrite(x, LOW);
+}
