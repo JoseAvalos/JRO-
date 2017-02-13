e07283215e2bd37efc7ac261211145e207008082
JoseAvalos <root@CI-38>
2017-02-13 17:45:41 -0500

    ..

--- SPI/SPI.ino
+++ SPI/SPI.ino
@@ -16,45 +16,22 @@ char* resp;
 DDS DDS_JRO(CS,UDCLK,IO_RESET,MRESET,SPI_module,SPI_delay);
 DDS_function DDS_tools;
 
-void setup() {
-
-  Serial.begin(115200);
+void setup() 
+{
+	Serial.begin(115200);
   Serial.println("#####################################");
-
 	DDS_JRO.init();
 	DDS_JRO.io_reset();
-  delayMicroseconds(SPI_delay);
-
-  // Verify the information
   resp=DDS_JRO.readData(0x87,4);
   DDS_tools.print(resp,4);
-  
   char* freq ;
-  freq = DDS_tools.freq2binary(5000000, SYSCLOCK);
+  freq = DDS_tools.freq2binary(20000000, SYSCLOCK);
 	DDS_JRO.writeData(0x02,6,freq);
-
-
- // delete[] resp;
-  delayMicroseconds(SPI_delay);
 	resp=DDS_JRO.readData(0x02,6);
 	DDS_tools.print(resp,6);
-	on(IO_RESET);
-	while(1);
 }
 
-void loop() {
+void loop() 
+{
 
-
-	
 }
-
-void on(int x) {
-  digitalWrite(x, HIGH);
-}
-
-void off(int x) {
-  digitalWrite(x, LOW);
-}
-
-
-
